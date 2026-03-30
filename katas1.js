752dd7fadb9ac25fa7843428743e0a4df2f6aad3
quonkelley <shaquonkelley@gmail.com>
2026-03-29 22:54:37 -0400

    fixed spaces

--- katas1.js
+++ katas1.js
@@ -1,22 +1,17 @@
 console.log("1 Display the numbers from 1 to 20. (1, 2, 3, …,19, 20)");  
 // Code Goes Here
 
-
 console.log("2 Display the even numbers from 1 to 20. (1, 3, 5, …, 17, 19)");  
 // Code Goes Here
-
 
 console.log("3 Display the odd numbers from 1 to 20. (1, 3, 5, …, 17, 19)");  
 // Code Goes Here
 
-
 console.log("4 Display the multiples of 5 up to 100. (5, 10, 15, …, 95, 100)");  
 // Code Goes Here
 
-
 console.log("5 Display the square numbers up to 100. (1, 4, 9, …, 81, 100)");  
 // Code Goes Here
-
 
 console.log("6 Display the numbers counting backwards from 20 to 1. (20, 19, 18, …, 2, 1)");  
 // Code Goes Here

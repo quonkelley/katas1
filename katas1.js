fd1acbb031af647aeab6b618eed4f3008d3b25b5
quonkelley <shaquonkelley@gmail.com>
2026-03-29 22:53:17 -0400

    katas1

---
+++ katas1.js
@@ -0,0 +1,37 @@
+console.log("1 Display the numbers from 1 to 20. (1, 2, 3, …,19, 20)");  
+// Code Goes Here
+
+
+console.log("2 Display the even numbers from 1 to 20. (1, 3, 5, …, 17, 19)");  
+// Code Goes Here
+
+
+console.log("3 Display the odd numbers from 1 to 20. (1, 3, 5, …, 17, 19)");  
+// Code Goes Here
+
+
+console.log("4 Display the multiples of 5 up to 100. (5, 10, 15, …, 95, 100)");  
+// Code Goes Here
+
+
+console.log("5 Display the square numbers up to 100. (1, 4, 9, …, 81, 100)");  
+// Code Goes Here
+
+
+console.log("6 Display the numbers counting backwards from 20 to 1. (20, 19, 18, …, 2, 1)");  
+// Code Goes Here
+
+console.log("7 Display the numbers counting backwards from 20 to 1. (20, 19, 18, …, 2");  
+// Code Goes Here
+
+console.log("8 Display the even numbers counting backwards from 20. (20, 18, 16, …, 4, 2)");  
+// Code Goes Here
+
+console.log("9 Display the odd numbers from 20 to 1, counting backwards. (19, 17, 15, …, 3, 1)");  
+// Code Goes Here
+
+console.log("10 Display the multiples of 5, counting down from 100. (100, 95, 90, …, 10, 5)");  
+// Code Goes Here
+
+console.log("Display the multiples of 5 up to 100. (5, 10, 15, …, 95, 100)");  
+// Code Goes Here
